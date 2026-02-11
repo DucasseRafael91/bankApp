33b1d3f323417694daeaeead032cf2d96926f6f3
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2026-02-11 11:28:54 +0100

    Creation du model OperationType

---
+++ src/com/bank/models/OperationType.java
@@ -0,0 +1,42 @@
+package com.bank.models;
+
+/**
+ * Entité TypeOperation.
+ */
+public class OperationType {
+  private int id;
+  private String type;
+
+  // Constructeur
+  /**
+   * Constructeur de TypeOperation.
+   */
+  public OperationType(Integer id, String type) {
+    this.id = id;
+    this.type = type;
+  }
+
+  @Override
+  public String toString() {
+    return type;
+  }
+
+  // Getters
+  public int getId() {
+    return id;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  // Setters
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public void setType(String type){
+    this.type = type;
+  }
+  
+}
